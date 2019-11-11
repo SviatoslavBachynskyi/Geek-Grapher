ab4bf0e9972be1fd501803dd2cfcc08bf75f3743
Rostyslav <rostyslavgarbar@gmail.com>
2019-11-11 16:55:25 +0200

    Added palette coloring

---
+++ FractalCore/ColorCalculators/PaletteCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+
+namespace GeekGrapher.FractalCore.ColorCalculators
+{
+    internal class PaletteCalculator : ColorCalculator
+    {
+        private Color[] palette;
+
+        public PaletteCalculator(Color[] palette)
+        {
+            this.palette = palette;
+        }
+
+        internal override Color Calculate(double iteration)
+        {
+            if (iteration == Drawer.MaxIteration) return Color.Black;
+
+            return palette[(int)iteration % palette.Length];
+        }
+    }
+}
