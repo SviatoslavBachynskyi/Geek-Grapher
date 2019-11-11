bf3b26d355c05c856781cd351d3e692a18e9f2d6
Sviatoslav Bachynskyi <44065082+SviatoslavBachynskyi@users.noreply.github.com>
2019-11-11 11:23:01 +0200

    added binding error logger (#25)
    * added binding error logger

--- GeekGrapher/App.xaml.cs
+++ GeekGrapher/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 
 namespace GeekGrapher
 {
@@ -13,5 +14,25 @@ namespace GeekGrapher
     /// </summary>
     public partial class App : Application
     {
+        StreamWriter _streamWriter;
+        BindingErrorListener _bindingErrorListener;
+        public App()
+        {
+#if DEBUG
+            _streamWriter = new StreamWriter(new FileStream("BindingErrors.txt", FileMode.Create));
+            _bindingErrorListener =
+            new BindingErrorListener(msg =>
+            {
+                _streamWriter.WriteLine(msg);
+            });
+#endif
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            _streamWriter?.Close();
+            _streamWriter?.Dispose();
+            _bindingErrorListener?.Dispose();
+        }
     }
 }
