00e6350319d3b0dd3b709e29180362fef793f2d9
Rostyslav <rostyslavgarbar@gmail.com>
2019-11-12 20:08:21 +0200

    Add image save but need fix bug(save image even error message is shown)

--- GeekGrapher/FractalPainter/Commands/SaveAsImage.cs
+++ GeekGrapher/FractalPainter/Commands/SaveAsImage.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Drawing;
+using System.Windows.Media.Imaging;
 
 namespace GeekGrapher.FractalPainter.Commands
 {
@@ -26,7 +30,62 @@ namespace GeekGrapher.FractalPainter.Commands
 
         public void Execute(object parameter)
         {
-            MessageBox.Show("Save");
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|Png Image|*.png";
+            saveFileDialog.Title = "Save an Image File";
+
+            saveFileDialog.ShowDialog();
+            BitmapEncoder encoder = null;
+
+            if (saveFileDialog.FileName != "")
+            {
+                switch (saveFileDialog.FilterIndex)
+                {
+                    case 1:
+                        encoder = new JpegBitmapEncoder();
+                        break;
+               
+                    case 2:
+                        encoder = new BmpBitmapEncoder();
+                        break;
+
+                    case 3:
+                        encoder = new GifBitmapEncoder();
+                        break;
+
+                    case 4:
+                        encoder = new PngBitmapEncoder();
+                        break;
+                }
+
+                var bitmapSource = _windowViewModel.Window.Image.Source as BitmapSource;
+
+                if (bitmapSource != null)
+                {
+                    Stream fileStream = (FileStream)saveFileDialog.OpenFile();
+                    byte[] imageByteArray = null;
+
+                    encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+
+                    using (var stream = new MemoryStream())
+                    {
+                        encoder.Save(stream);
+                        imageByteArray = stream.ToArray();
+                    }
+
+                    fileStream.Write(imageByteArray, 0, imageByteArray.Length);
+                    fileStream.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Nothing to save!");
+                    return;
+                }
+                
+            }
+
         }
     }
 }
