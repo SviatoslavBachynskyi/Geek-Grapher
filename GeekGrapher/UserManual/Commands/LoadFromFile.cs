99f086d9240284dfc9f26bc77a3194520821d253
Sviatoslav Bachynskyi <44065082+SviatoslavBachynskyi@users.noreply.github.com>
2019-12-10 17:43:22 +0200

    Feature/manual (#54)
    * made loading of .rtf files to Richtextbox

---
+++ GeekGrapher/UserManual/Commands/LoadFromFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using GeekGrapher.General;
+using GeekGrapher.UserManual.ViewModels;
+
+namespace GeekGrapher.UserManual.Commands
+{
+    class LoadFromFile : ChangableCommand
+    {
+        private ManualViewModel _manualViewModel;
+        public LoadFromFile(ManualViewModel manualViewModel)
+        {
+            _manualViewModel = manualViewModel;
+        }
+        public override void Execute(object parameter)
+        {
+            string path = (string)parameter;
+            string text;
+            try
+            {
+                text = File.ReadAllText(path);
+            }
+            catch (IOException)
+            {
+                MessageBox.Show(
+                    "Не вдалося прочитати файл з інструкцією, переконайтеся, що програма містить всі файли",
+                    "Помилка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                text = _manualViewModel.Text;
+            }
+
+            _manualViewModel.Text = text;
+        }
+    }
+}
